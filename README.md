cec3b59bf740edff128ff9ee3c679f0aeebaf89a
microttus <martino@uia.no>
2024-01-29 17:20:03 +0100

    Backbone of code ready to test

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # IceTeslaGlove
-A package for ROS2 control of the IceTeslaGlove
+
+A package for ROS2 control of the IceTeslaGlove. 
+Simply launch the executable to launch the node, and control will be enabled.
+For control og multiple gloved id have to be set individually. 
+Support for multiple devices without code configurations will be added later.
+
+## How to Use
+
+
+
+
+
+## Package configuration
+
+Below are the class diagram for the package shown. 
+
+![IceTeslaGlove class diagram](./images/IceTeslaGlove.png)
--- src/ice_tesla_glove.cpp
+++ src/ice_tesla_glove.cpp
@@ -13,7 +13,7 @@
 
 using namespace std::chrono_literals;
 
-struct FingerForceMeasured{
+struct HandDouble{
   double thumb;
   double index;
   double middle;
@@ -22,7 +22,7 @@ struct FingerForceMeasured{
   double palm;
 };
 
-struct IceGloveServoPos{
+struct HandServoPos{
   int thumb;
   int index;
   int middle;
@@ -36,7 +36,10 @@ class IceTeslaGlove : public rclcpp::Node
  public:
   IceTeslaGlove()
       : Node("ice_tesla_glove")
-      , RobotHandForce{0,0,0,0,0,0}
+      , RobotFingerForce{0, 0, 0, 0, 0, 0}
+      , OperatorFingerPos{0,0,0,0,0,0}
+      , ServoMultiplierNormToPos{0,0,0,0,0,0}
+      , ForceFeedbackNormToPos{0,0,0,0,0,0}
       , ServoPosGloveOne{0,0,0,0,0,0}
   {
     pub_servo_glove_pos_ = this->create_publisher<geometry_msgs::msg::Twist>("ice_glove_id1", 10);
@@ -44,6 +47,7 @@ class IceTeslaGlove : public rclcpp::Node
         10ms, std::bind(&IceTeslaGlove::timer_callback, this));
 
     robot_force_sub_1_ = this->create_subscription<geometry_msgs::msg::Twist>("/robot_force_id1", 10, std::bind(&IceTeslaGlove::update_force_feedback, this, std::placeholders::_1));
+   operator_pos_sub_1_ = this->create_subscription<geometry_msgs::msg::Twist>("/operator_pos_id1", 10, std::bind(&IceTeslaGlove::update_hand_pos_feedback, this, std::placeholders::_1));
   }
 
  private:
@@ -55,22 +59,42 @@ class IceTeslaGlove : public rclcpp::Node
 
   void calculate_servo_pos()
   {
-    // Read normalized servo value
+    // Read normalized stretch value.........
     // convert normalized value to servo angle
     // add force feedback
     // write to servo struct
+
+    ServoPosGloveOne.thumb = static_cast<int>((OperatorFingerPos.thumb*ServoMultiplierNormToPos.thumb)-(RobotFingerForce.thumb*ForceFeedbackNormToPos.thumb));
+    ServoPosGloveOne.index = static_cast<int>((OperatorFingerPos.index*ServoMultiplierNormToPos.index)-(RobotFingerForce.index*ForceFeedbackNormToPos.index));
+    ServoPosGloveOne.middle = static_cast<int>((OperatorFingerPos.middle*ServoMultiplierNormToPos.middle)-(RobotFingerForce.middle*ForceFeedbackNormToPos.middle));
+    ServoPosGloveOne.ring = static_cast<int>((OperatorFingerPos.ring*ServoMultiplierNormToPos.ring)-(RobotFingerForce.ring*ForceFeedbackNormToPos.ring));
+    ServoPosGloveOne.little = static_cast<int>((OperatorFingerPos.little*ServoMultiplierNormToPos.little)-(RobotFingerForce.little*ForceFeedbackNormToPos.little));
+    ServoPosGloveOne.palm = static_cast<int>((OperatorFingerPos.palm*ServoMultiplierNormToPos.palm)-(RobotFingerForce.palm*ForceFeedbackNormToPos.palm));
   }
 
+  // Read and updates the force exerted ion the fingertip of the robot and
   void update_force_feedback(geometry_msgs::msg::Twist::SharedPtr msg)
   {
-    RobotHandForce.thumb = msg->linear.x;
-    RobotHandForce.index = msg->linear.y;
-    RobotHandForce.middle = msg->linear.z;
-    RobotHandForce.ring = msg->angular.x;
-    RobotHandForce.little = msg->angular.y;
-    RobotHandForce.palm = msg->angular.z;
+    RobotFingerForce.thumb = msg->linear.x;
+    RobotFingerForce.index = msg->linear.y;
+    RobotFingerForce.middle = msg->linear.z;
+    RobotFingerForce.ring = msg->angular.x;
+    RobotFingerForce.little = msg->angular.y;
+    RobotFingerForce.palm = msg->angular.z;
   }
 
+  // Read and updates the pos feedback from the load cell of the operator glove
+  void update_hand_pos_feedback(geometry_msgs::msg::Twist::SharedPtr msg)
+  {
+    OperatorFingerPos.thumb = msg->linear.x;
+    OperatorFingerPos.index = msg->linear.y;
+    OperatorFingerPos.middle = msg->linear.z;
+    OperatorFingerPos.ring = msg->angular.x;
+    OperatorFingerPos.little = msg->angular.y;
+    OperatorFingerPos.palm = msg->angular.z;
+  }
+
+  // Write the updated servo position values to the glove topic
   void update_servo_pos()
   {
     auto servo_msg = geometry_msgs::msg::Twist();
@@ -85,12 +109,17 @@ class IceTeslaGlove : public rclcpp::Node
     pub_servo_glove_pos_->publish(servo_msg);
   }
 
-  FingerForceMeasured RobotHandForce;
-  IceGloveServoPos ServoPosGloveOne;
+  HandDouble RobotFingerForce;
+  HandDouble OperatorFingerPos;
+  HandDouble ServoMultiplierNormToPos;
+  HandDouble ForceFeedbackNormToPos;
+
+  HandServoPos ServoPosGloveOne;
 
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_servo_glove_pos_;
   rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr robot_force_sub_1_;
+  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr operator_pos_sub_1_;
   //size_t count_{};
 };
 
