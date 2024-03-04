c43719e20c982b21efa66b904667227b5e68b028
microttus <martino@uia.no>
2024-03-04 14:23:15 +0100

    First working system

--- src/ice_tesla_glove.cpp
+++ src/ice_tesla_glove.cpp
@@ -37,6 +37,120 @@ struct HandServoPos{
   int palm;
 };
 
+class FileImport
+{
+ public:
+  FileImport()
+  = default;
+
+  void import_hand_profiles(const std::string& name_of_profile, HandDouble& MinStruct, HandDouble& MaxStruct) {
+    // Open the CSV file
+    std::ifstream file("src/ice_tesla_glove/tools/icecube_tesla_glove_profiles.csv");
+
+    // Check if the file is open
+    if (!file.is_open()) {
+      std::cerr << "Error opening Operator file." << std::endl;
+      return;
+    }
+
+    // Define a 2D vector to store the data
+    std::vector<std::vector<std::string>> data;
+
+    // Read each line from the CSV file
+    std::string line;
+    while (std::getline(file, line)) {
+      // Use a string stream to split the line into tokens
+      std::istringstream iss(line);
+      std::vector<std::string> tokens;
+
+      // Read each token separated by commas
+      std::string token;
+      while (std::getline(iss, token, ',')) {
+        tokens.push_back(token);
+      }
+
+      // Add the tokens to the data vector
+      data.push_back(tokens);
+    }
+
+    // Close the file
+    file.close();
+
+    // Save Operator hand info
+    for (auto &row : data) {
+      if (row[0] == name_of_profile) {
+        MinStruct.thumb = std::stod(row[2]);
+        MaxStruct.thumb = std::stod(row[1]);
+        MinStruct.index = std::stod(row[3]);
+        MaxStruct.index = std::stod(row[4]);
+        MinStruct.middle = std::stod(row[5]);
+        MaxStruct.middle = std::stod(row[6]);
+        MinStruct.ring = std::stod(row[7]);
+        MaxStruct.ring = std::stod(row[8]);
+
+      }
+    }
+  }
+
+  void import_operator_glove_pos_settings(HandDouble& MinStruct, HandDouble& MaxStruct){
+    // Open the CSV file
+    std::ifstream file("src/ice_tesla_glove/tools/operator_finger_pos_cal.csv");
+
+    // Check if the file is open
+    if (!file.is_open()) {
+      std::cerr << "Error opening glove pos settings file." << std::endl;
+      return;
+    }
+
+    // Define a 2D vector to store the data
+    std::vector<std::vector<std::string>> data;
+
+    // Read each line from the CSV file
+    std::string line;
+    while (std::getline(file, line)) {
+      // Use a string stream to split the line into tokens
+      std::istringstream iss(line);
+      std::vector<std::string> tokens;
+
+      // Read each token separated by commas
+      std::string token;
+      while (std::getline(iss, token, ',')) {
+        tokens.push_back(token);
+      }
+
+      // Add the tokens to the data vector
+      data.push_back(tokens);
+    }
+
+    // Close the file
+    file.close();
+
+    for (auto &row : data) {
+      if (row[0] == "thumb") {
+        MaxStruct.thumb = std::stod(row[1]);
+        MinStruct.thumb = std::stod(row[2]);
+      } else if (row[0] == "index") {
+        MaxStruct.index = std::stod(row[1]);
+        MinStruct.index = std::stod(row[2]);
+      } else if (row[0] == "middle") {
+        MaxStruct.middle = std::stod(row[1]);
+        MinStruct.middle = std::stod(row[2]);
+      } else if (row[0] == "ring") {
+        MaxStruct.ring = std::stod(row[1]);
+        MinStruct.ring = std::stod(row[2]);
+      } else if (row[0] == "little") {
+        MaxStruct.little = std::stod(row[1]);
+        MinStruct.little = std::stod(row[2]);
+      } else if (row[0]  == "palm") {
+        MaxStruct.palm = std::stod(row[1]);
+        MinStruct.palm = std::stod(row[2]);
+      } else {
+        RCLCPP_INFO(rclcpp::get_logger("IceTeslaGlove"), "Operator Glove settings have settings not implemented");
+      }
+    }
+  }
+};
+
 class IceTeslaGlove : public rclcpp::Node
 {
  public:
@@ -45,9 +159,11 @@ class IceTeslaGlove : public rclcpp::Node
       , RobotFingerForce{0, 0, 0, 0, 0, 0}
       , OperatorFingerPos{0,0,0,0,0,0}
       , OperatorFingerPosServo{0,0,0,0,0,0}
+      , ForceFeedbackNormToPos{0,0,0,0,0,0}
       , ServoMultiplierNormToPosMin{0,0,0,0,0,0}
       , ServoMultiplierNormToPosMax{0,0,0,0,0,0}
-      , ForceFeedbackNormToPos{0,0,0,0,0,0}
+      , OperatorPositionMin{0,0,0,0,0,0}
+      , OperatorPositionMax{0,0,0,0,0,0}
       , ServoPosGloveOne{0,0,0,0,0,0}
       , name_of_profile("proto_1")
   {
@@ -64,7 +180,12 @@ class IceTeslaGlove : public rclcpp::Node
     robot_force_sub_1_ = this->create_subscription<geometry_msgs::msg::Twist>("/robot_force_id1", 10, std::bind(&IceTeslaGlove::update_force_feedback, this, std::placeholders::_1));
     operator_pos_sub_1_ = this->create_subscription<geometry_msgs::msg::Twist>("/ice_glove_pos_id1", micro_ros_qos_profile, std::bind(&IceTeslaGlove::update_hand_pos_feedback, this, std::placeholders::_1));
 
-    import_hand_profiles();
+    // Use FileImport class for updating the settings
+    FileImporter.import_hand_profiles(name_of_profile, ServoMultiplierNormToPosMin, ServoMultiplierNormToPosMax);
+    FileImporter.import_operator_glove_pos_settings(OperatorPositionMin, OperatorPositionMax);
+
+    std::signal(SIGINT, &IceTeslaGlove::onShutdown);
+
   }
 
  private:
@@ -72,6 +193,17 @@ class IceTeslaGlove : public rclcpp::Node
   {
     calculate_servo_pos();
     update_servo_pos();
+  }
+
+  static void onShutdown(int signum) {
+    if (signum == SIGINT) {
+      // Perform cleanup operations before shutting down
+      RCLCPP_INFO(rclcpp::get_logger("IceTeslaGlove"), "Ice gone, meltdown begun...");
+      // Add your cleanup logic here
+
+      // Call the ROS 2 shutdown function
+      rclcpp::shutdown();
+    }
   }
 
   void calculate_servo_pos()
@@ -106,12 +238,12 @@ class IceTeslaGlove : public rclcpp::Node
   // Read and updates the pos feedback from the load cell of the operator glove
   void update_hand_pos_feedback(geometry_msgs::msg::Twist::SharedPtr msg)
   {
-    OperatorFingerPos.thumb = to_norm_val_with_guard(float(msg->linear.x), 3700.0f, 1300.0f, OperatorFingerPos.thumb);
-    OperatorFingerPos.index = to_norm_val_with_guard(float(msg->linear.y), 3000.0f, 950.0f, OperatorFingerPos.index);
-    OperatorFingerPos.middle = to_norm_val_with_guard(float(msg->linear.z), 3200.0f, 1900.f, OperatorFingerPos.middle);
-    OperatorFingerPos.ring = to_norm_val_with_guard(float(msg->angular.x), 3000.0f, 1000.0f, OperatorFingerPos.ring);
-    OperatorFingerPos.little = to_norm_val_with_guard(float(msg->angular.y), 3000.0f, 1000.0f, OperatorFingerPos.little);
-    OperatorFingerPos.palm = to_norm_val_with_guard(float(msg->angular.z), 1.0f, 0.0f, OperatorFingerPos.palm);
+    OperatorFingerPos.thumb = to_norm_val_with_guard(float(msg->linear.x), OperatorPositionMax.thumb, OperatorPositionMin.thumb, OperatorFingerPos.thumb);
+    OperatorFingerPos.index = to_norm_val_with_guard(float(msg->linear.y), OperatorPositionMax.index, OperatorPositionMin.index, OperatorFingerPos.index);
+    OperatorFingerPos.middle = to_norm_val_with_guard(float(msg->linear.z), OperatorPositionMax.middle, OperatorPositionMin.middle, OperatorFingerPos.middle);
+    OperatorFingerPos.ring = to_norm_val_with_guard(float(msg->angular.x), OperatorPositionMax.ring, OperatorPositionMin.ring, OperatorFingerPos.ring);
+    OperatorFingerPos.little = to_norm_val_with_guard(float(msg->angular.y), OperatorPositionMax.little, OperatorPositionMin.little, OperatorFingerPos.little);
+    OperatorFingerPos.palm = to_norm_val_with_guard(float(msg->angular.z), OperatorPositionMax.palm, OperatorPositionMin.palm, OperatorFingerPos.palm);
   }
 
   // Write the updated servo position values to the glove topic
@@ -130,12 +262,13 @@ class IceTeslaGlove : public rclcpp::Node
   }
 
   // Function to normalize position values with guard for outliers
-  static float to_norm_val_with_guard(float input_val, float upper_val, float lower_val, float last_val=0){
-      if (input_val > upper_val or input_val < lower_val){
-        return last_val;
-      } else {
-        return (input_val - lower_val) / (upper_val - lower_val);
-      }
+  static double to_norm_val_with_guard(float input_val, double upper_val, double lower_val, double last_val=0){
+    auto d_input_val = static_cast<double>(input_val);
+    if (d_input_val > upper_val or d_input_val < lower_val) {
+      return last_val;
+    } else {
+      return (d_input_val - lower_val) / (upper_val - lower_val);
+    }
   }
 
   double map_output(double in_val, double out_min, double out_max){
@@ -152,69 +285,26 @@ class IceTeslaGlove : public rclcpp::Node
     OperatorFingerPosServo.palm = map_output(OperatorFingerPos.palm, ServoMultiplierNormToPosMin.palm, ServoMultiplierNormToPosMax.palm);
   }
 
-  void import_hand_profiles(){
-    // Open the CSV file
-    std::ifstream file("icecube_tesla_glove_profiles.csv");
-
-    // Check if the file is open
-    if (!file.is_open()) {
-      std::cerr << "Error opening file." << std::endl;
-      return;
-    }
-
-    // Define a 2D vector to store the data
-    std::vector<std::vector<std::string>> data;
-
-    // Read each line from the CSV file
-    std::string line;
-    while (std::getline(file, line)) {
-      // Use a stringstream to split the line into tokens
-      std::istringstream iss(line);
-      std::vector<std::string> tokens;
-
-      // Read each token separated by commas
-      std::string token;
-      while (std::getline(iss, token, ',')) {
-        tokens.push_back(token);
-      }
-
-      // Add the tokens to the data vector
-      data.push_back(tokens);
-    }
-
-    // Close the file
-    file.close();
-
-    // Display the read data
-    for (auto& row : data) {
-      if (row[0] == name_of_profile) {
-        ServoMultiplierNormToPosMin.thumb = std::stod(row[2]);
-        ServoMultiplierNormToPosMax.thumb = std::stod(row[1]);
-        ServoMultiplierNormToPosMin.index = std::stod(row[3]);
-        ServoMultiplierNormToPosMax.index = std::stod(row[4]);
-        ServoMultiplierNormToPosMin.middle = std::stod(row[5]);
-        ServoMultiplierNormToPosMax.middle = std::stod(row[6]);
-        ServoMultiplierNormToPosMin.ring = std::stod(row[7]);
-        ServoMultiplierNormToPosMax.ring = std::stod(row[8]);
-
-      }
-    }
-  }
 
   HandDouble RobotFingerForce;
   HandDouble OperatorFingerPos;
   HandDouble OperatorFingerPosServo;
-  HandDouble ServoMultiplierNormToPosMin;
-  HandDouble ServoMultiplierNormToPosMax;
   HandDouble ForceFeedbackNormToPos;
 
+  HandDouble ServoMultiplierNormToPosMin;
+  HandDouble ServoMultiplierNormToPosMax;
+  HandDouble OperatorPositionMin;
+  HandDouble OperatorPositionMax;
+
   HandServoPos ServoPosGloveOne;
+
+  FileImport FileImporter;
 
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_servo_glove_pos_;
   rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr robot_force_sub_1_;
   rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr operator_pos_sub_1_;
-  //size_t count_{};
+
   std::string name_of_profile;
 };
 
@@ -222,7 +312,18 @@ class IceTeslaGlove : public rclcpp::Node
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<IceTeslaGlove>());
+
+  auto node =  std::make_shared<IceTeslaGlove>();
+
+  // Create a SingleThreadedExecutor
+  rclcpp::executors::SingleThreadedExecutor executor;
+
+  // Add your node to the executor
+  executor.add_node(node);
+
+  // Run the executor
+  executor.spin();
+
   rclcpp::shutdown();
   return 0;
 }
